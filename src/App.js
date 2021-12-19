02d9e353ffcb91d2d72982d5ea0ab625941fb6bb
unknown <ksm1538@naver.com>
2021-12-19 15:14:30 +0900

    publish_using_input_

--- src/App.js
+++ src/App.js
@@ -7,24 +7,51 @@ function App (){
   let [current_no, current_no_change] = useState(0);
   let [recommendCnt, recommendCnt_change] = useState(0);
   let [modal_check, modal_check_change] = useState(false);
+  let [user_input, user_input_change] = useState("");
 
   function currentNoChange(num){
     current_no_change(num)
   }
+
+  function add(){
+    if(user_input == ""){
+      alert("입력하세요");
+    }else{
+      var array = [...state_title];
+      array.push(user_input);
+      //array.shift(user_input); 
+      
+      // push : 배열 뒤에 추가
+      // unshift : 배열 앞에 추가
+      // shift : 배열 앞에서 삭제
+      
+      state_title_change(array);
+
+      var array=[...date_];
+      array.push("2021-12-25");
+      date_change(array);
+    }
+  }
+
 
   return (
     <div className="App">
       <div className="yellow-nav">
           <div>SM's HowToUseReact </div>
       </div>  
+      <div className="publish">
+        제목 : <input onChange={ (e)=>{ user_input_change(e.target.value) } }/>
+        <button onClick={()=>{ add() }}>저장</button>
+
+      </div>
       <div className="div-list">
 
       {
         state_title.map(function(item, cnt){
           return(
-          <div className="div-list">
+          <div className="div-list" key={cnt}>
             <h3 onClick={ ()=>{currentNoChange(cnt)} }>{ item }<span onClick={ ()=>{recommendCnt_change(recommendCnt+1)} }> ❤ </span>{ recommendCnt }</h3>
-            <p>2021-12-18</p>
+            <p>{date_[cnt]}</p>
             <hr/>
           </div>
           )
@@ -43,9 +70,10 @@ function App (){
 function Modal(props){
   return (
     <div className="modal">
-      <h2>제목 { props.state_title[props.current_no] }</h2>
-      <p>날짜 { props.date_[props.current_no]} </p>
-      <p>상세내용</p>
+      <h1>Detail</h1>
+      <h2>제목 : { props.state_title[props.current_no] }</h2>
+      <p>날짜 : { props.date_[props.current_no]} </p>
+      <p>상세내용 : </p>
     </div>
   )
 }
