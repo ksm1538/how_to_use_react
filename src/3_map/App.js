a8f5bb4b558855fd246c4fd5882d58539d505a03
unknown <ksm1538@naver.com>
2021-12-19 14:57:52 +0900

    map_and_state

--- src/3_map/App.js
+++ src/3_map/App.js
@@ -1,39 +1,29 @@
 import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-
+function App (){
   let [state_title, state_title_change] = useState(["지금 날씨는 영하 9도네요.","This is State", "오늘 풋살합니다~"]);
-  let [recommendCnt, recommendCnt_change] = useState(0);
   let [date_, date_change] = useState(["2021-12-17","2021-12-18","2021-12-19"]);
+  let [current_no, current_no_change] = useState(0);
+  let [recommendCnt, recommendCnt_change] = useState(0);
   let [modal_check, modal_check_change] = useState(false);
-  
+
+  function currentNoChange(num){
+    current_no_change(num)
+  }
 
   return (
     <div className="App">
       <div className="yellow-nav">
-        <div>SM's HowToUseReact </div>
+          <div>SM's HowToUseReact </div>
       </div>  
       <div className="div-list">
-      {/*
-        <h3>{ state_title[0] }<span onClick={ ()=>{recommendCnt_change(recommendCnt+1)} }> ❤ </span>{ recommendCnt }</h3>
-        <p>2021-12-17</p>
-        <hr/>
 
-        <h3>{ state_title[1] }</h3>
-        <p>2021-12-18</p>
-        <hr/>
-
-        <h3>{ state_title[2] }</h3>
-        <p>2021-12-18</p>
-        <hr/>
-        */}
-      
       {
-        state_title.map(function(item){
+        state_title.map(function(item, cnt){
           return(
           <div className="div-list">
-            <h3>{ item }<span onClick={ ()=>{recommendCnt_change(recommendCnt+1)} }> ❤ </span>{ recommendCnt }</h3>
+            <h3 onClick={ ()=>{currentNoChange(cnt)} }>{ item }<span onClick={ ()=>{recommendCnt_change(recommendCnt+1)} }> ❤ </span>{ recommendCnt }</h3>
             <p>2021-12-18</p>
             <hr/>
           </div>
@@ -41,19 +31,22 @@ function App() {
         })
       }
 
+        <button onClick={ ()=>{currentNoChange(0)} }>버튼 1</button>
+        <button onClick={ ()=>{currentNoChange(1)} }>버튼 2</button>
+        <button onClick={ ()=>{currentNoChange(2)} }>버튼 3</button>
+        <Modal state_title={state_title} date_={date_} current_no={current_no}></Modal>
       </div>
-      <button onClick={ ()=>{modal_check_change(!modal_check)} }>모달 on/off</button>
-      { modal_check == true ? <Modal>contents={state_title}</Modal> : null  }
-    </div>
-  );
+    </div>  
+  )
 }
 
 function Modal(props){
-
   return (
-      <div className="modal">
-      안뇽
-      </div>
+    <div className="modal">
+      <h2>제목 { props.state_title[props.current_no] }</h2>
+      <p>날짜 { props.date_[props.current_no]} </p>
+      <p>상세내용</p>
+    </div>
   )
 }
 
--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import './App.css';
 function App (){
   let [state_title, state_title_change] = useState(["지금 날씨는 영하 9도네요.","This is State", "오늘 풋살합니다~"]);
   let [date_, date_change] = useState(["2021-12-17","2021-12-18","2021-12-19"]);
+  let [current_no, current_no_change] = useState(0);
+  let [recommendCnt, recommendCnt_change] = useState(0);
+  let [modal_check, modal_check_change] = useState(false);
+
+  function currentNoChange(num){
+    current_no_change(num)
+  }
 
   return (
     <div className="App">
@@ -11,7 +18,23 @@ function App (){
           <div>SM's HowToUseReact </div>
       </div>  
       <div className="div-list">
-        <Modal state_title={state_title} date_={date_}></Modal>
+
+      {
+        state_title.map(function(item, cnt){
+          return(
+          <div className="div-list">
+            <h3 onClick={ ()=>{currentNoChange(cnt)} }>{ item }<span onClick={ ()=>{recommendCnt_change(recommendCnt+1)} }> ❤ </span>{ recommendCnt }</h3>
+            <p>2021-12-18</p>
+            <hr/>
+          </div>
+          )
+        })
+      }
+
+        <button onClick={ ()=>{currentNoChange(0)} }>버튼 1</button>
+        <button onClick={ ()=>{currentNoChange(1)} }>버튼 2</button>
+        <button onClick={ ()=>{currentNoChange(2)} }>버튼 3</button>
+        <Modal state_title={state_title} date_={date_} current_no={current_no}></Modal>
       </div>
     </div>  
   )
@@ -20,8 +43,8 @@ function App (){
 function Modal(props){
   return (
     <div className="modal">
-      <h2>제목 { props.state_title[0] }</h2>
-      <p>날짜 { props.date_[0]} </p>
+      <h2>제목 { props.state_title[props.current_no] }</h2>
+      <p>날짜 { props.date_[props.current_no]} </p>
       <p>상세내용</p>
     </div>
   )
